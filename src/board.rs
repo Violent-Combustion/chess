eca5a5dbb39b99c9b1fe4e55188afac44a70f7ab
Seth Grissman <seth.grissman.2@gmail.com>
2024-03-08 17:17:14 +0000

    Added board struct and piece trait

--- src/board.rs
+++ src/board.rs
@@ -1,1 +1,7 @@
 // Contains the board, and manages the logic.
+
+struct Board {
+    grid: [ [ option; 8 ]; 8 ],
+    // turn: ColorEnum,
+    game_over: bool,
+}
--- src/piece.rs
+++ src/piece.rs
@@ -1,1 +1,5 @@
-// Piece trait and all associated methods will go here.
+trait Piece {
+    fn build( )-> self;
+    fn kill( &self );
+    fn validate_move( &self, ( i32, i32 ) )-> bool;
+}
