229a260bd4fb391b45d0ee8efcd6d94b25110fb5
Seth Grissman <seth.grissman.2@gmail.com>
2024-04-29 14:35:02 +0000

    WHY ARE PATTERNS NOT ALLOWED

--- src/variants/mod.rs
+++ src/variants/mod.rs
@@ -14,4 +14,8 @@ pub enum PieceColor {
     White,
 }
 
+trait Piece {
+    fn verify_move( ( x, y ): ( usize, usize ) )-> bool; //WHY ARE PATTERNS NOT PERMITTED HERE?!?!
+}
+
 pub mod pawn;
--- src/variants/pawn.rs
+++ src/variants/pawn.rs
@@ -1,1 +1,11 @@
-pub struct Pawn;
+pub struct Pawn {
+    potential_moves: [ ( usize, usize ); 1 ],
+}
+
+impl Pawn {
+    pub fn build_default( )-> Pawn {
+        Pawn {
+            potential_moves: [ ( 0, 1 ) ],
+        }
+    }
+}
